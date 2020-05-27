7ba40408f71b8e96ffd193114cf48a7f0a301c1c
ADiks <antondikson@gmail.com>
2020-05-27 13:23:50 +0300

    finish base framowork

---
+++ src/components/table/table.template.js
@@ -0,0 +1,50 @@
+const CODES = {
+	A: 65,
+	Z: 90,
+};
+const toCell = () => '<div class="cell" contenteditable ></div>';
+
+const createCells = (countCols) => {
+	const cells = new Array(countCols)
+		.fill('')
+		.map(toCell)
+		.join('');
+	return cells;
+};
+
+const toColumn = (char) => {
+	return `
+		<div class="column">${char}</div>
+	`;
+};
+
+const createRow = (content, index = '') => {
+	return `
+		<div class="row">
+			<div class="row-info">${index}</div>
+			<div class="row-data">${content}</div>
+		</div>
+	`;
+};
+
+const toChar = (_, index) => {
+	return String.fromCharCode(CODES.A + index);
+};
+
+export function createTable(countRows = 20) {
+	const countCols = CODES.Z - CODES.A + 1;
+	const rows = [];
+	const cols = new Array(countCols)
+		.fill('')
+		.map(toChar)
+		.map(toColumn)
+		.join('');
+
+	rows.push(createRow(cols));
+
+	for (let i = 0; i < countRows; i++) {
+		rows.push(createRow(createCells(countCols), i + 1));
+	}
+
+	return rows.join('');
+}
